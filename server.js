ba80a6561020890882c3721ee61c9b85651353bd
theguyfromtv <goldfingerorama@gmail.com>
2018-06-08 11:28:53 -0400

    Building for production

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+}*/
 
 /*app.get('*', function (request, response){
   response.sendFile('index.html')
