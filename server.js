95c1be102c5d2f4cc6337d7d3f1692742ef4b3e4
theguyfromtv <goldfingerorama@gmail.com>
2018-06-08 14:23:27 -0400

    Building for production

--- server.js
+++ server.js
@@ -25,12 +25,15 @@ app.use(passport.session());
 
 
 // Add  API routes
-app.use("/api",routes);
 
 
 //make the server serve up react's index file and use react router
 app.get('*', function (req, res){
-  res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  if (req.route.startsWith('/api')){
+    app.use(routes);
+  }else{
+    res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  }
 })
 
 
