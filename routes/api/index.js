191bb2875a153c73b6a0d7dfc94a90c9dfc3859d
theguyfromtv <goldfingerorama@gmail.com>
2018-06-08 13:58:53 -0400

    Building for production

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,6 @@ const chatRoutes = require('./chats')
 
 router.use("/auth", authRoutes);
 router.use('/chats',chatRoutes)
-router.use("/login", userRoutes);
+router.use("/", userRoutes);
 
 module.exports = router;
