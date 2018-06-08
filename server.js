b62d451fbebb1e632707ed24b8b61644dbdde510
theguyfromtv <goldfingerorama@gmail.com>
2018-06-08 14:35:59 -0400

    Building for production

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(passport.session());
 
 //make the server serve up react's index file and use react router
 app.get('*', function (req, res){
-  console.log(req.route.path)
+  console.log(req.route)
   if (req.route.path.startsWith('/api')){
     app.use(routes);
   }else{
