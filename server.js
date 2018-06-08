7a3f53f62ef8e368118ee8c710c39433934aa4d4
theguyfromtv <goldfingerorama@gmail.com>
2018-06-08 09:55:44 -0400

    Building for production

--- server.js
+++ server.js
@@ -18,12 +18,12 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+/*if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-}
+}*/
 
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname,'index.html'))
+  response.sendFile(path.resolve('client/build/index.html'))
 })
 
 // set up session cookies
