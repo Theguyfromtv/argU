2dcc2952f874938022a6bcc963348fd441d20953
theguyfromtv <goldfingerorama@gmail.com>
2018-06-10 08:42:26 -0400

    Building for production

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -49,6 +49,7 @@ class Chat extends Component {
   render() {
     return (
     <div className="col-sm-8">
+      <div className="overflow">
         <div className="row">
           <div id="chatView">
             {this.state.messages.map((message,index)=>{
@@ -72,6 +73,7 @@ class Chat extends Component {
           </div>
 
         </div>
+      </div>
       </div>
     )
   }
