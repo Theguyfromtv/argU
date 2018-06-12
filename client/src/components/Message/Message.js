275006730f73d896bafbc22099d15e7712f2f3ce
theguyfromtv <goldfingerorama@gmail.com>
2018-06-12 14:20:30 -0400

    Building for production

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -4,17 +4,23 @@ import "./Message.css"
  const Message=(props)=> {
     return (
       <div className="message">
-          {props.message.sender===props.user._id?
-          <div style={{float:"right"}}>
-            <p>{props.message.side?"Pro":"Con"}</p>
-            <div className={"card "+props.className} >
-              <div className="card-body">
-                <p>{props.message.message}</p>
+          {props.message.sender==props.user._id?
+          <div className="row">
+            <div className="col-lg-4 col-sm-10">
+              <div>
+              <p>{props.message.side?"Pro":"Con"}</p>
+              <div className={"card "+props.className} >
+                <div className="card-body">
+                  <p>{props.message.message}</p>
+                </div>
               </div>
+          </div>
             </div>
           </div>
           :
-          <div style={{float:"left"}}>
+          <div className="row">
+            <div className="col-lg-8 col-sm-2">
+            <div>
             <p>{props.message.side?"Pro":"Con"}</p>
             <div className="card message" >
               <div className="card-body">
@@ -22,6 +28,9 @@ import "./Message.css"
               </div>
             </div>
           </div>
+            </div>
+          </div>
+          
           }
 
       
