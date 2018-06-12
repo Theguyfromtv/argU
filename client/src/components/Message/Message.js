1a99a11aa603ff2161305b8233b73287f276943b
theguyfromtv <goldfingerorama@gmail.com>
2018-06-12 09:52:22 -0400

    Building for production

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
  const Message=(props)=> {
     return (
       <div>
-        <h6>{props.message.side?"Pro":"Against"}</h6>
+        <h6>Message</h6>
         <div className="card message" style={{backgroundColor: props.color}}>
           <div className="card-body">
             <p>{props.message.message}</p>
