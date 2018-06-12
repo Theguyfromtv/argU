388536f34edcb1ef39e0c4ca159a6d1995ca7fa0
theguyfromtv <goldfingerorama@gmail.com>
2018-06-12 15:18:12 -0400

    Building for production

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -6,6 +6,8 @@ import "./Message.css"
       <div className="message">
           {props.message.sender==props.user._id?
           <div className="row">
+            <div className="col-lg-8 col-sm-2">
+            </div>
             <div className="col-lg-4 col-sm-10">
               <div>
               <p>{props.message.side?"Pro":"Con"}</p>
@@ -19,15 +21,15 @@ import "./Message.css"
           </div>
           :
           <div className="row">
-            <div className="col-lg-8 col-sm-2">
-            <div>
+            <div className="col-lg-4 col-sm-10">
             <p>{props.message.side?"Pro":"Con"}</p>
             <div className="card message" >
               <div className="card-body">
                 <p>{props.message.message}</p>
               </div>
             </div>
-          </div>
+            </div>
+            <div className="col-lg-8 col-sm-2">
             </div>
           </div>
           
