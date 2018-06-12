dd655e710e9c6480ee3fd914d5062afc7e11f6df
theguyfromtv <goldfingerorama@gmail.com>
2018-06-12 18:57:25 -0400

    Building for production

--- client/src/components/Logo/Logo.js
+++ client/src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <div>
-      <img src="https://image.ibb.co/bUNaA8/agrulogo.png" alt="argu.chat" className="img-responsive align-center" style={{margin:"auto"}}/>
+      <img src="https://image.ibb.co/bUNaA8/agrulogo.png" alt="argu.chat" className="img-responsive align-center" style={{margin:"auto", marginBottom:"0"}}/>
     </div>
   )
 }
