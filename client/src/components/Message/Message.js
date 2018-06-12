7255146fa0610accbaab632ab4b48f598ca6970e
theguyfromtv <goldfingerorama@gmail.com>
2018-06-12 11:44:31 -0400

    Building for production

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -4,13 +4,35 @@ import "./Message.css"
  const Message=(props)=> {
     return (
       <div className="message">
-        <div>
-          <h6>{props.message.side?"Pro":"Con"}</h6>
-          <div className="card message" style={{backgroundColor: props.color}}>
-            <div className="card-body">
-              <p>{props.message.message}</p>
+        <div className="row">
+          {props.message.id===props.user._id?
+          <div>
+            <div className="col-lg-8 col-sm-2">
+            </div>
+          <div className="col-lg-4 col-sm-10">
+            <h6>{props.message.side?"Pro":"Con"}</h6>
+            <div className="card message" style={{backgroundColor: props.color}}>
+              <div className="card-body">
+                <p>{props.message.message}</p>
+              </div>
             </div>
           </div>
+          </div>
+          :
+          <div>
+          <div className="col-lg-4 col-sm-10">
+            <h6>{props.message.side?"Pro":"Con"}</h6>
+            <div className="card message" style={{backgroundColor: props.color}}>
+              <div className="card-body">
+                <p>{props.message.message}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-8 col-sm-2">
+          </div>
+          </div>
+          }
+
         </div>
       </div>
     )
