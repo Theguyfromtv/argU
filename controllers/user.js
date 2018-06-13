ea24b51bff09ddd7d3a4d948e10ca2a2e194cf4c
theguyfromtv <goldfingerorama@gmail.com>
2018-06-13 05:17:03 -0400

    Building for production

--- controllers/user.js
+++ controllers/user.js
@@ -35,46 +35,49 @@ userController.updateTopics=(req,res)=>{
     //find one who disagrees on the same topic and create a promise that creates a new chat on the db
     User.findOne({topics:{$elemMatch:{topic:newTopic,side:!newSide}}},function(err,user2){
       console.log(user2)
-      if(user1.email===user2.email){
-        res.send("no matches")
-      }
-      else if(user1.email!==user2.email){
-        if(newSide){
-          participant1=user1;
-          participant2=user2;
-       }else if(!newSide){
-          participant2=user1;
-          participant1=user2;
-       }
-       if(user2){
-           /*let coinflip= Math.floor(Math.random() * 2);
-           let turn
-           if(coinflip==1){
-             turn=true
-           }else if (coinflip==0){
-             turn=false
-           }*/
-           //creating the chat. sending the first message letting them know they matched and then adding the new chat to the user's files on the db, then sending back a response with the updated user object
-           Chat.findOrCreate({participant1id:participant1._id, participant1name:participant1.name,participant1read:false, participant2id:participant2._id, participant2name:participant2.name,participant2read:false,isActive:true,topic:newTopic, messages:[]},function(err,chat){
-             if (err) throw err
-             let chatId=chat._id
- 
-             socket.emit('match', chat)
-               
-             User.findOneAndUpdate({_id:user1._id},{$addToSet:{chats:chatId}},function(err,user){
+      if(user2){
+        if(user1.email===user2.email){
+          res.send("no matches")
+        }
+        else if(user1.email!==user2.email){
+          if(newSide){
+            participant1=user1;
+            participant2=user2;
+         }else if(!newSide){
+            participant2=user1;
+            participant1=user2;
+         }
+         if(user2){
+             /*let coinflip= Math.floor(Math.random() * 2);
+             let turn
+             if(coinflip==1){
+               turn=true
+             }else if (coinflip==0){
+               turn=false
+             }*/
+             //creating the chat. sending the first message letting them know they matched and then adding the new chat to the user's files on the db, then sending back a response with the updated user object
+             Chat.findOrCreate({participant1id:participant1._id, participant1name:participant1.name,participant1read:false, participant2id:participant2._id, participant2name:participant2.name,participant2read:false,isActive:true,topic:newTopic, messages:[]},function(err,chat){
                if (err) throw err
-               User.findOneAndUpdate({_id:user2._id},{$addToSet:{chats:chatId}},function(err,user){
-                 if (err) throw err            
-                 res.json(chat)
+               let chatId=chat._id
+   
+               socket.emit('match', chat)
+                 
+               User.findOneAndUpdate({_id:user1._id},{$addToSet:{chats:chatId}},function(err,user){
+                 if (err) throw err
+                 User.findOneAndUpdate({_id:user2._id},{$addToSet:{chats:chatId}},function(err,user){
+                   if (err) throw err            
+                   res.json(chat)
+                 })
                })
+   
              })
- 
-           })
-       }else{
-         console.log(user1)
-         res.send("no matches")
-       }
- 
+         }else{
+           console.log(user1)
+           res.send("no matches")
+         }
+   
+      }
+      
  
      }
  
